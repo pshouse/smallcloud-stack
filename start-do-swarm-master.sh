9b608cdf083c19dcaeb799f63840b25b05726922
pshouse <shouse.patrick@gmai.com>
2016-01-03 22:35:25 -0500

    add subdirecty to proxy_pass. add docker-machine scripts

---
+++ start-do-swarm-master.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker-machine create --driver digitalocean \
+	--digitalocean-access-token $DO_TOKEN\
+	--swarm --swarm-master \
+	--swarm-discovery="consul://$KEYSTORE_IP:8500" \
+	--engine-opt="cluster-store=consul://$KEYSTORE_IP:8500" \
+ 	--engine-opt="cluster-advertise=eth0:2376" swarm-master
